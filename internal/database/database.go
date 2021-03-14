fb3d0e32684725f9084f0f657f26f8306ecf7f7b
Oluwatobi Adeyinka <adeyinka.oluwatobi@yahoo.com>
2021-03-14 22:08:20 +0100

    Removed redundant type

--- internal/database/database.go
+++ internal/database/database.go
@@ -5,7 +5,5 @@ import (
 	"gorm.io/gorm"
 )
 
-type DbHandler struct{}
-
 var dsn = "appuser:password@tcp(127.0.0.1:3307)/todo_api?charset=utf8mb4&parseTime=True&loc=Local"
 var DB, _ = gorm.Open(mysql.Open(dsn), &gorm.Config{})
